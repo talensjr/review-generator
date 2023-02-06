b865ea3a004f07e7f07f145742a2d6e6d5f9f7ad
Jordi Talens <talensjr@gmail.com>
2023-02-06 08:26:29 +0100

    chore: remove token

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,42 +1,6 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
-
   COHERE_API_KEY: string;
 }
-
-// curl --location --request POST 'https://api.cohere.ai/generate' \
-//   --header 'Authorization: BEARER hgxvkhaqsJOMH2K26nnOZHtDlbmukpwNAC2OjaRJ' \
-//   --header 'Content-Type: application/json' \
-//   --header 'Cohere-Version: 2022-12-06' \
-//   --data-raw '{
-//       "model": "command-xlarge-20221108",
-//       "prompt": "Write a 5 out of 5 stars review comment for a laptop named \"macbook air\" highlighting \"battery\" and \"price\".",
-// "max_tokens": 100,
-// "temperature": 1.2,
-// "k": 0,
-// "p": 0.75,
-// "frequency_penalty": 0,
-// "presence_penalty": 0,
-// "stop_sequences": [],
-// "return_likelihoods": "NONE"
-//     }'
 
 function getOptions({ COHERE_API_KEY }) {
   return {
